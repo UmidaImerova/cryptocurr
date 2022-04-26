984b73a940c14033db2f57cd5e10325787de3657
UmidaImerova <umidagaliamova@gmail.com>
2022-04-26 09:31:24 +0600

    restructuring store fpr redux

--- src/store/store.jsx
+++ src/store/store.jsx
@@ -1,11 +1,13 @@
 import {configureStore} from '@reduxjs/toolkit'
 import topCoinSlice from  '../data/topCoinSlice'
 import coinSlice from  '../data/coinSlice'
+import newsSlice from '../data/newsSlice'
 
 const store =  configureStore({
     reducer:{
         topCrypto:topCoinSlice.reducer,
-        cryptocurr:coinSlice.reducer
+        cryptocurr:coinSlice.reducer,
+        news: newsSlice.reducer
 
     }
 })
